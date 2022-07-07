3aa151c71f0f691a6f748091223d997a9b6e0987
jsi19 <jonathan.si0619@gmail.com>
2022-07-07 12:31:03 -0700

    Added new profile picture and description with Scroll view effect

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import * as React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, SafeAreaView, ScrollView, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.photoContainer}>
-        <Image source={require("./assets/profile.png")} style={styles.photo} />
+        <Image source={require("./assets/squareprofile.png")} style={styles.photo} />
       </View>
       <View style={styles.nameContainer}>
         <Text style={styles.nameAndPronouns}>Jonathan! (he/him)</Text>
       </View>
-      <View style={styles.blurbContainer}>
-        <Text style={styles.blurb}>bark bark ahhhh grrrrrrr!!!</Text>
-      </View>
+      <ScrollView style={styles.blurbContainer}>
+        {/* <Text style={styles.blurb}>I am currently a recent Computer Science graduate from Rio Hondo College who will be transferring to UCLA in Fall 2022.</Text>
+        <Text style={styles.blurb}>I am passionate about pursuing my major in Software Development and Computer Science.</Text> 
+        <Text style={styles.blurb}>This summer, I will be participating, as one of the 15 selected scholars, in the Snap Engineering Academy hosted by Snap Inc. at it’s headquarters in Santa Monica, CA.</Text> 
+       */}
+        <Text style={styles.blurb}>
+        I am currently a recent Computer Science graduate from Rio Hondo College who will be transferring to UCLA in Fall 2022. 
+        I am passionate about pursuing my major in Software Development and Computer Science. 
+        This summer, I will be participating, as one of the 15 selected scholars, in the Snap Engineering Academy hosted by Snap Inc. at it’s headquarters in Santa Monica, CA.
+        </Text>
+        <Text></Text>
+        <Text style={styles.blurb}>
+        I enjoy creating opportunities out of misfortunes, researching and developing new technologies, designing software applications for various platforms. 
+        After interning with NASA last summer, I learned how to use microelectronics and software development tools to leverage existing technologies to solve problems affecting our communities. 
+        This summer within the Snap Engineering Academy, I will continue to learn and develop my skills in software engineering! 
+        </Text>
+        <Text></Text>
+        <Text style={styles.blurb}>
+        I am passionate about leveraging technology, advancing my skills, and creating new innovations.
+        </Text>
+      </ScrollView>
     </View>
   );
 }
@@ -22,31 +40,35 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   photoContainer: {
-    flex: 3,
+    flex: 2,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
+    backgroundColor: "#00a6b5",
     padding: 30,
   },
   nameContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#2f80b0",
   },
   blurbContainer: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
+    flex: 4,
+    // justifyContent: "center",
+    // alignItems: "center",
+    backgroundColor: "#4f569f",
   },
   photo: {
     resizeMode: "contain",
-    width: "60%",
+    width: "100%",
     padding: 50,
   },
   nameAndPronouns: {
     fontSize: 40,
+    color: "white",
   },
   blurb: {
     fontSize: 20,
+    color: "white",
   },
 });
